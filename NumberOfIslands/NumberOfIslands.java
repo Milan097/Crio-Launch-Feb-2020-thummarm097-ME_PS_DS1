0814b86d2ff90710e0d7e2ee409e7e39c8e4156e
Milan097 <thummarm097@gmail.com>
2020-03-18 08:02:20 +0000

    [LocalTest]:[NumberOfIslands]:[java]

--- NumberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/NumberOfIslands.java
@@ -3,56 +3,31 @@ import java.util.*;
 
 class NumberOfIslands {
 
-    public static void bfs(char[][] grid,int x,int y,int n,int m) {
-        int curx=x,cury=y;
-        // System.out.println(curx + " " + cury);
-        if((curx >= m-1) && (cury >= n-1))
-            return;
-        if(curx >= m-1) {
-            curx = m-1;
-            if(grid[curx][cury + 1] == '0')
-                return;
-            if(grid[curx][cury+1] == '1') {
-                grid[curx][cury+1] = '2';
-                bfs(grid,curx,cury+1,n,m);
-            }
-        }
-        else if(cury >= n-1) {
-            cury = n-1;
-            if(grid[curx+1][cury] == '0')
-                return;
-            if(grid[curx+1][cury] == '1') {
-                grid[curx+1][cury] = '2';
-                bfs(grid,curx+1,cury,n,m);
-            }
-        }
-        else {
-            if(grid[curx][cury+1] == '0' && grid[curx+1][cury] == '0') 
-                return;
-            if(grid[curx][cury+1] == '1') {
-                grid[curx][cury+1] = '2';
-                bfs(grid,curx,cury+1,n,m);
-            }
-            if(grid[curx+1][cury] == '1') {
-                grid[curx+1][cury] = '2'; 
-                bfs(grid,curx+1,cury,n,m);
-            }
-        }
-    }
+    public void bfs(int i,int j,char arr[][]){
+	    arr[i][j] = '0';
+	    for(int k=0;k<4;k++){
+	        int x = i + GetXAndY.xAxis[k];
+	        int y = j + GetXAndY.yAxis[k];
+	        
+	        if(x<0 || x>=arr.length || y<0 || y>= arr[0].length)
+	            continue;
+	        if(arr[x][y] == '1')
+                bfs(x,y,arr);
+	    }
+	}
 
     // Implement your solution by completing the below function
     public int numIslands(char[][] grid) {
         int n = grid[0].length;
         int m = grid.length;
-        if(m == 0 && n == 0)
+        if(m == 0 || n == 0)
             return 0; 
         int cnt = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if(grid[i][j] == '1') {
-                    // System.out.println(cnt+" "+i+" "+j);
                     cnt++;
-                    bfs(grid,i,j,n,m);
+                    bfs(i,j,grid);
                 }
             }
         } 
