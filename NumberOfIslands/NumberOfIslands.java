002e061975783765e372a48c7d014746242cc2ab
Milan097 <thummarm097@gmail.com>
2020-03-18 07:36:42 +0000

    [LocalTest]:[NumberOfIslands]:[java]

--- NumberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/NumberOfIslands.java
@@ -3,16 +3,33 @@ import java.util.*;
 
 class NumberOfIslands {
 
-    public static void bfs(char[][] grid,int curx,int cury) {
-        while(true) {
+    public static void bfs(char[][] grid,int x,int y,int n) {
+        int curx=x,cury=y;
+        if(curx >= n-1 && cury >= n-1)
+            return;
+        if(curx >= n-1) {
+            curx = n-1;
+            if(grid[curx][cury + 1] == '0')
+                return;
+            if(grid[curx][cury+1] == '1')
+                grid[curx][cury+1] = '2';
+        }
+        else if(cury >= n-1) {
+            cury = n-1;
+            if(grid[curx+1][cury] == '0')
+                return;
+            if(grid[curx+1][cury] == '1')
+                grid[curx+1][cury] = '2';
+        }
+        else {
             if(grid[curx][cury+1] == '0' && grid[curx+1][cury] == '0') 
                 return;
             if(grid[curx][cury+1] == '1')
                 grid[curx][cury+1] = '2';
             if(grid[curx+1][cury] == '1')
                 grid[curx+1][cury] = '2';
-            bfs(grid,curx,cury+1);
-            bfs(grid,curx+1,cury);
+            bfs(grid,curx,cury+1,n);
+            bfs(grid,curx+1,cury,n);
         }
     }
 
@@ -24,7 +41,7 @@ class NumberOfIslands {
             for(int j=0;j<n;j++) {
                 if(grid[i][j] == '1') {
                     cnt++;
-                    bfs(grid,i,j);
+                    bfs(grid,i,j,n);
                 }
             }
         } 
