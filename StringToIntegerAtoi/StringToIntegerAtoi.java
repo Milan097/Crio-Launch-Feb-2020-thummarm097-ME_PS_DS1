168e1810436383e223b84c8458a2e59d7e779183
Milan097 <thummarm097@gmail.com>
2020-03-18 08:36:07 +0000

    [LocalTest]:[StringToIntegerAtoi]:[java]

--- StringToIntegerAtoi/StringToIntegerAtoi.java
+++ StringToIntegerAtoi/StringToIntegerAtoi.java
@@ -21,7 +21,7 @@ class StringToIntegerAtoi {
         }
         int ans = 0;
         while(ind < n) {
-            System.out.println(str.charAt(ind) + " " + ans);
+            System.out.print(str.charAt(ind));
             if(str.charAt(ind) == ' ' || str.charAt(ind) == '\t')
                 ind++;
             else if(str.charAt(0) == '-') {
@@ -36,7 +36,7 @@ class StringToIntegerAtoi {
                 ans += (str.charAt(ind) - '0');
                 ind++;
             }
-            
+            System.out.println(" " + ans);
         }
         if(pre == 0)
             return ans;
