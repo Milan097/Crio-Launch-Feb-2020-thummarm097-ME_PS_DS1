972d9ff5671f1a4184315cae5cc96c67c27385b7
Milan097 <thummarm097@gmail.com>
2020-03-18 07:48:14 +0000

    [LocalTest]:[NumberOfIslands]:[java]

--- NumberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/NumberOfIslands.java
@@ -12,27 +12,31 @@ class NumberOfIslands {
             curx = n-1;
             if(grid[curx][cury + 1] == '0')
                 return;
-            if(grid[curx][cury+1] == '1')
+            if(grid[curx][cury+1] == '1') {
                 grid[curx][cury+1] = '2';
-            bfs(grid,curx,cury+1,n);
+                bfs(grid,curx,cury+1,n);
+            }
         }
         else if(cury >= n-1) {
             cury = n-1;
             if(grid[curx+1][cury] == '0')
                 return;
-            if(grid[curx+1][cury] == '1')
+            if(grid[curx+1][cury] == '1') {
                 grid[curx+1][cury] = '2';
-            bfs(grid,curx+1,cury,n);
+                bfs(grid,curx+1,cury,n);
+            }
         }
         else {
             if(grid[curx][cury+1] == '0' && grid[curx+1][cury] == '0') 
                 return;
-            if(grid[curx][cury+1] == '1')
+            if(grid[curx][cury+1] == '1') {
                 grid[curx][cury+1] = '2';
-            if(grid[curx+1][cury] == '1')
-                grid[curx+1][cury] = '2';
-            bfs(grid,curx,cury+1,n);
-            bfs(grid,curx+1,cury,n);
+                bfs(grid,curx,cury+1,n);
+            }
+            if(grid[curx+1][cury] == '1') {
+                grid[curx+1][cury] = '2'; 
+                bfs(grid,curx+1,cury,n);
+            }
         }
     }
 
