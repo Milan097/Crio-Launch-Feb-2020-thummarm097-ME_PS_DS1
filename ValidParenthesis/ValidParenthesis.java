a9ef86000afb8348ecc3b3a9832b054877899342
Milan097 <thummarm097@gmail.com>
2020-03-18 09:20:28 +0000

    [Solved]:[ValidParenthesis]:[java]

--- ValidParenthesis/ValidParenthesis.java
+++ ValidParenthesis/ValidParenthesis.java
@@ -3,15 +3,27 @@ import java.util.*;
 
 public class ValidParenthesis {
     // Implement your solution by completing the below function
-    public boolean isValid(String s) {
-
-		return false;
+  public boolean isValid(String s) {
+    int n = s.length();
+    int cnt = 0;
+    for(int i=0;i<n;i++) {
+      if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+        cnt++;
+      } else {
+        cnt--;
+      }
+      if(cnt < 0)
+        return false;
     }
+    if(cnt == 0)
+      return true;
+    return false;
+  }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String parenthesis = in.readLine();
-        boolean result = new ValidParenthesis().isValid(parenthesis);
-        System.out.print(String.valueOf(result));
-    }
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    String parenthesis = in.readLine();
+    boolean result = new ValidParenthesis().isValid(parenthesis);
+    System.out.print(String.valueOf(result));
+  }
 }
