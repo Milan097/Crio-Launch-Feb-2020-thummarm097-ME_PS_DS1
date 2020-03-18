e4b2d6fae3168b94661461c8028ec19807f4be16
Milan097 <thummarm097@gmail.com>
2020-03-18 07:29:25 +0000

    [LocalTest]:[NumberOfIslands]:[java]

--- NumberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/NumberOfIslands.java
@@ -3,10 +3,32 @@ import java.util.*;
 
 class NumberOfIslands {
 
+    public static void bfs(char[][] grid,int curx,int cury) {
+        while(1) {
+            if(grid[curx][cury+1] == '0' && grid[curx+1][cury] == '0') 
+                return;
+            if(grid[curx][cury+1] == '1')
+                grid[curx][cury+1] = '2';
+            if(grid[curx+1][cury] == '1')
+                grid[curx+1][cury] = '2';
+            bfs(grid,curx,cury+1);
+            bfs(grid,curx+1,cury);
+        }
+    }
+
     // Implement your solution by completing the below function
     public int numIslands(char[][] grid) {
-
-        return 0;
+        int n = grid[0].length;
+        int cnt = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == '1') {
+                    cnt++;
+                    bfs(grid,i,j);
+                }
+            }
+        } 
+        return cnt;
     }
 
     public static void main(String[] args) {
